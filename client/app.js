76c03ceffedba2bfafe16e587408e88ef36819c7
iissnan <iissnan@gmail.com>
2015-03-10 13:43:17 +0800

    UI: Edit/Remove environments

--- client/app.js
+++ client/app.js
@@ -52,18 +52,41 @@ angular.module('at', [])
         env.isEditing = true;
       };
 
-      $scope.cancelEdit = function (env) {
+      $scope.cancel = function (env) {
         env.isEditing = false;
       };
 
       $scope.save = function (env) {
         app.updateEnv({
-          id: id,
-          name: envName,
-          url: envUrl,
-          accountStr: accounts
+          id: env.id,
+          name: env.name,
+          url: env.url,
+          accountStr: env.accountStr
         });
+
+        var index = 0;
+        $scope.environments.forEach(function (e, i) {
+          if (e.id === env.id) {
+            index = i;
+          }
+        });
+        $scope.environments[index] = env;
+
+        env.isEditing = false;
       };
+
+      $scope.remove = function (env) {
+        app.removeEnv(env.id);
+
+        var index = 0;
+        $scope.environments.forEach(function (e, i) {
+          if (e.id === env.id) {
+            index = i;
+          }
+        });
+        $scope.environments.splice(index, 1);
+      };
+
     }])
     ;
 
@@ -81,43 +104,6 @@ function startBrowser (event) {
     action: url + '/login/main.asp'
   });
 }
-
-$(document).ready(function () {
-  $('.btn-edit').on('click', function () {
-    var $panel = $(this).parents('.panel');
-    $panel.find('.account-edit').removeClass('hidden');
-    $panel.find('.account-list').addClass('hidden');
-    $panel.find('.env-name').trigger('click');
-  });
-
-  $('.bnt-remove').on('click', function () {
-    var $panel = $(this).parents('.panel');
-    var id = $panel.attr('id');
-    app.removeEnv(id);
-    $panel.find('.env-name').trigger('click');
-  });
-
-  $('.btn-save').on('click', function () {
-    var $panel = $(this).parents('.panel');
-    var id = $panel.attr('id');
-    var envName = $panel.find('.account-edit-name input').val();
-    var envUrl = $panel.find('.account-edit-url input').val();
-    var accounts = $panel.find('.account-edit-accounts').val();
-
-    app.updateEnv({
-      id: id,
-      name: envName,
-      url: envUrl,
-      accountStr: accounts
-    });
-  });
-
-  $('.btn-danger').on('click', function () {
-    var $panel = $(this).parents('.panel');
-    $panel.find('.account-list').removeClass('hidden');
-    $panel.find('.account-edit').addClass('hidden');
-  });
-});
 
 $(window).on('keyup', function(e){
   switch (e.keyCode){
