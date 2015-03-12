1af3f94097259aa92c2edd8a1dd0e1428e9c6c9b
trump <wangzhenchuan@gmail.com>
2015-03-12 18:49:45 +0800

    remove gulp

--- app/livereload.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-
-gulp.task('reload', function () {
-  location && location.reload();
-});
-
-gulp.watch('app/**/*.html', ['reload']);
-gulp.watch('app/app.js', ['reload']);
